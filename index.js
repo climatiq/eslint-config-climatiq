8a8937ad7ebf660b977a1344190ae7a47fda66cc
Ignacio Cruz <igmoweb@gmail.com>
2022-08-03 15:38:47 +0200

    Ignore Empty functions warning

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     'no-console': 2,
     '@typescript-eslint/ban-ts-comment': 1,
-    '@typescript-eslint/no-unused-vars': 2
+    '@typescript-eslint/no-unused-vars': 2,
+    "@typescript-eslint/no-empty-function": "off",
   }
 }
