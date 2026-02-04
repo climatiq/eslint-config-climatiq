6e0802541820d79d6d3f517803a6f316659e97ae
Ignacio Cruz <igmoweb@gmail.com>
2026-02-04 08:11:20 +0100

    Upgrade packages

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended-type-checked', 'prettier'],
-  plugins: [
-    // This plugin automatically removes unused imports
-    "unused-imports"
-  ],
-  rules: {
-    'curly': 'error',
-    // Sometimes ts-ignore is handy if we justify it
-    '@typescript-eslint/ban-ts-comment': [
-      'warn',
-      {
-        'ts-check': 'allow-with-description',
-        'ts-expect-error': 'allow-with-description',
-        'ts-ignore': 'allow-with-description',
-        'ts-nocheck': 'allow-with-description',
-      },
-    ],
-    "@typescript-eslint/no-empty-function": "off",
-    '@typescript-eslint/no-unused-vars': 'off', // delegate to plugin below that handles both vars and imports
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'all',
-        caughtErrorsIgnorePattern: '^_'
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['**/*.test.ts', '**/*/test-stubs.ts', '**/*/test-helpers.ts'],
-      rules: {
-        // Handy to have all tests be async even if we don't use it in them
-        '@typescript-eslint/require-await': 'off',
-        // No need to be super strict with anys in test code
-        // Also these don't seem to work well with snapshot matchers
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-      },
-    },
-    {
-      //see https://typescript-eslint.io/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
-      // This is to avoid errors about this not being in the TSConfig
-      extends: ['plugin:@typescript-eslint/disable-type-checked'],
-      files: ['./**/*.js'],
-    },
-    {
-      files: ['**/*.tsx', '**/*.jsx'],
-      rules: {
-        'no-console': 'error' // Disable no-console rule for .tsx files
-      }
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  {
+    rules: {
+      curly: "error",
+      // Sometimes ts-ignore is handy if we justify it
+      "@typescript-eslint/ban-ts-comment": [
+        "warn",
+        {
+          "ts-check": "allow-with-description",
+          "ts-expect-error": "allow-with-description",
+          "ts-ignore": "allow-with-description",
+          "ts-nocheck": "allow-with-description"
+        }
+      ],
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": "off", // delegate to plugin below that handles both vars and imports
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_"
+        }
+      ]
     }
-  ],
-}
+  },
+  {
+    plugins: {
+      // This plugin automatically removes unused imports
+      "unused-imports": unusedImports
+    }
+  },
+  {
+    files: ["**/*.tsx", "**/*.jsx"],
+    rules: {
+      "no-console": "error" // Disable no-console rule for .tsx files
+    }
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    ".vercel/**"
+  ])
+]);
